6d57c9d0980ae56295e1a1f375f5f08cecfe6502
John Dulaney <john.s.dulaney1@gmail.com>
2018-03-08 11:58:49 -0600

    :construction: Added some proposed modules to be developed later.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import palm from './GlobalModules/palm.png';
 import './App.css';
 
 class App extends Component {
@@ -7,11 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={palm} className="App-logo" alt="logo" />
+          <h1 className="App-title">Whats up idiot? Welcome to Triplann.</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        I drink your milkshake timothy dalton funny walk mexican casual style Fallen eyebrow basil fawlty sam elliott, funny walk great dictator casual style wario mexican middle eastern despot burt reynolds timothy dalton Fallen eyebrow I drink your milkshake casual style sam elliott basil fawlty, timothy dalton Fallen eyebrow basil fawlty I drink your milkshake mexican casual style middle eastern despot great dictator casual style louis xiii sam elliott burt reynolds face mop consectetur wario funny walk. Decathlon champion dick van dyke dali godlike dolor sit amet freestyle, cripes derek griffiths dick van dyke dolor sit amet godlike holiday waiter decathlon champion freestyle dali east european dali, david seaman lando calrissian landed gentry dick van dyke handsome decathlon champion zap rowsdower freestyle dali mexican magnum pi cripes dolor sit amet derek griffiths godlike louis xiii dali holiday waiter east european.
         </p>
       </div>
     );
---
+++ src/GlobalModules/loginForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import palm from './palm.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={palm} className="App-logo" alt="logo" />
+          <h1 className="App-title">Whats up idiot? Welcome toTriplann</h1>
+        </header>
+        <p className="App-intro">
+        
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
