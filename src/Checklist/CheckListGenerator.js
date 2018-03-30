66dd50b703fbf3c106f826ccffa8c1c547e7cccf
John Dulaney <john.s.dulaney1@gmail.com>
2018-03-30 17:32:25 -0400

    few mc

--- src/Checklist/CheckListGenerator.js
+++ src/Checklist/CheckListGenerator.js
@@ -1,13 +1,15 @@
 // import { Component } from 'React';
+// import { log } from 'util';
 
 // class CheckListGenerator extends Component {
 //     constructor(props)
 //         super(props)
 //         this.state = {
-//             ''
+//             🚧  Fixed a few random bugs that hindered demo day. When will our button get deleted? 🙍‍♂️🚧  Fixed a few random bugs that hindered demo day. When will our button get deleted? 🙍‍♂️
 //         }
-
+//         🚧  Fixed a few random bugs that hindered demo day. When will our button get deleted? 🙍‍♂️🚧  Fixed a few random bugs that hindered demo day. When will our button get deleted? 🙍‍♂️🚧  Fixed a few random bugs that hindered demo day. When will our button get deleted? 🙍‍♂️🚧  Fixed a few random bugs that hindered demo day. When will our button get deleted? 🙍‍♂️🚧  Fixed a few random bugs that hindered demo day. When will our button get deleted? 🙍‍♂️🚧  Fixed a few random bugs that hindered demo day. When will our button get deleted? 🙍‍♂️🚧  Fixed a few random bugs that hindered demo day. When will our button get deleted? 🙍‍♂️🚧  Fixed a few random bugs that hindered demo day. When will our button get deleted? 🙍‍♂️🚧  Fixed a few random bugs that hindered demo day. When will our button get deleted? 🙍‍♂️
 //         componentDidMount () {
+//             console.log(asdafasdfsadf);
             
 //         }
         
